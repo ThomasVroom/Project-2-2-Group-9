294bc7ba8b1ac9d798cb6c62527a419d9acc114c
tjip1234 <rafaeldiederen@gmail.com>
2023-03-08 15:07:22 +0100

    some stuff

---
+++ src/main/java/org/Project22/Matching/Match1.java
@@ -0,0 +1,4 @@
+package org.Project22.Matching;
+
+public class Match1 {
+}
---
+++ src/main/java/org/Project22/Matching/MatchingInterface.java
@@ -0,0 +1,7 @@
+package org.Project22.Matching;
+
+import org.Project22.Question;
+
+public interface MatchingInterface {
+        float Matching(String userQuestion, Question question);
+}
