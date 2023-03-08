9087f761688e16c1908e427743c54cd4d7d81897
guiott67 <thierry.guiot.001@student.uni.lu>
2023-03-08 11:41:25 +0100

    work on skillHandle

--- src/main/java/org/Project22/Main.java
+++ src/main/java/org/Project22/Main.java
@@ -1,52 +1,9 @@
 package org.Project22;
 
-import java.io.IOException;
-import java.util.*;
+import org.Project22.GUI.UI;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String[] fileNames = new String[]{"activities.txt","drinks.txt","food.txt","lectures.txt","services.txt","theater.txt"};
-        List<SkillHandle> skills = new ArrayList<>();
-        List<Question> questions = new ArrayList<>();
-        for (String filename: fileNames) {
-            skills.add(new SkillHandle(filename));
-        }
-        for (SkillHandle skill:skills) {
-            questions.add(skill.returnQuestion());
-        }
-        System.out.println(matchQuestion("what things are available at the park while its monday",questions));
+    public static void main(String[] args){ 
+        new UI();
     }
-
-    /**
-     * @param userString question of the user
-     * @param Questions list of question aka skills of the chatbot
-     * @return an ordered list with (index of question,algorithm used + name of question, matches percentage)
-     */
-    public static List<Triple<Integer,String,Float>> matchQuestion(String userString, List<Question> Questions, int... algorithm){
-        List<Triple<Integer,String,Float>> result = new ArrayList<>();
-        int choice = 2;
-        if (algorithm.length >= 1){
-             choice = algorithm[0];
-        }
-
-        for (Question question : Questions) {
-            if (choice == 0)
-                result.add(new Triple<>(Questions.indexOf(question),"matches1.0 ,"+question.name,question.Matches(userString)));
-            else if (choice == 1)
-                result.add(new Triple<>(Questions.indexOf(question),"matches2.0 ,"+question.name,question.Matches2(userString)));
-            else if (choice == 2)
-                result.add(new Triple<>(Questions.indexOf(question),"matches3.0 ,"+question.name,question.Matches3(userString)));
-            else
-                throw new IllegalArgumentException("choose a valid algorithm choice");
-        }
-        Comparator<Triple<Integer, String, Float>> comparator = new Comparator<Triple<Integer, String, Float>>() {
-            @Override
-            public int compare(Triple<Integer, String, Float> triple1, Triple<Integer, String, Float> triple2) {
-                return Float.compare(triple2.z(),triple1.z());
-            }
-        };
-        Collections.sort(result, comparator);
-        return result;
-    }
-
 }
--- src/main/java/org/Project22/SkillHandle.java
+++ src/main/java/org/Project22/SkillHandle.java
@@ -1,61 +1,121 @@
 package org.Project22;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import org.Project22.*;
 
 public class SkillHandle {
-    public String BigString;
-    public String filename;
-    public SkillHandle(String filename) throws IOException {
-        this.filename = filename;
-        BigString = TextHandler.readFileToString("resources/SkillFiles/"+filename);
-        //System.out.println(BigString);
+
+    //***** don't know what to do with this
+    List<Tuple<String,List<String>>> answers = new ArrayList<>();
+    //*****
+
+    private Question question = null;
+    String skillID = "";
+    private String skillQuestion = new String();
+    private List<Tuple<String,String>> slots = new ArrayList<>();
+    private List<Tuple<String,List<Tuple<String,String>>>> skillAnswers = new ArrayList<>();
+
+
+    public SkillHandle(String filename){
+        this.skillID = filename.substring(0, filename.lastIndexOf('.'));
+
+        try {
+            ReadLines(filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.question = new Question(skillQuestion, slots, skillAnswers);
     }
 
-    public Question returnQuestion(){
-        List<String> slots = new ArrayList<>();
-        List<String> answers = new ArrayList<>();
-        String question = null;
-        String[] lines = BigString.split("\n");
-        for (String line : lines) {
-            if (line.startsWith("question")||line.startsWith("Question"))
-                question = line.substring(9);
-            else if (line.startsWith("slot")||line.startsWith("Slot"))
-                slots.add(line.substring(4));
-            else if (line.startsWith("answer")||line.startsWith("Answer"))
-                answers.add(line.substring(7));
-            else if (line.trim().length()>0)
-                System.out.println("Unrecognised String:" + line);
-        }
-        List<Tuple<String,String>> slotsExtracted = new ArrayList<>();
-        for (String slot:slots) {
-            slotsExtracted.add(new Tuple<>(slot.substring(slot.indexOf("<")+1,slot.indexOf(">")).trim().toUpperCase(),slot.substring(slot.indexOf(">")+1).trim()));
-        }
-        List<Tuple<String,List<Tuple<String,String>>>> answersFinal = new ArrayList<>();
-        for (String answer: answers){
-            List<Tuple<String,String>> placeholders = getVariable(answer,slotsExtracted);
-            while (answer.contains("<")){
-                answer = answer.replaceAll(answer.substring(answer.indexOf("<"),answer.indexOf(">")+1), "");
-            }
-            answersFinal.add(new Tuple<>(answer,placeholders));
-        }
-        return new Question(question,slotsExtracted,answersFinal,filename);
+    public Question getQuestion(){
+        return this.question;
     }
-    public List<Tuple<String,String>> getVariable(String answer,List<Tuple<String,String>> slotsExtracted){
-        List<String> userWords = Arrays.asList(answer.split(" "));
-        List<Tuple<String,String>> result = new ArrayList<>();
-        List<String> categories = new ArrayList<>();
-        for (String word: userWords) {
-            for (Tuple<String,String> slot: slotsExtracted) {
-                if(word.equals(slot.y()) && !categories.contains(slot.x())){
-                    result.add(slot);
-                    categories.add(slot.x());
-                    break;
-                }
+
+
+    private void ReadLines(String filename) throws FileNotFoundException, IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                ParseLine(line);
+            }
+            reader.close();
+        }
+    }
+
+    private void ParseLine(String line){
+
+        if (line.length() < 6) {
+            System.out.println("Invalid line was ignored");
+        }
+        else
+
+        if (line.substring(0, 8).equals("Question")) {
+            this.skillQuestion = ParseQuestion(line);
+        } 
+
+        else if (line.substring(0, 6).equals("Answer")) {
+            this.skillAnswers.add(ParseAnswer(line));
+        }
+
+        else if (line.substring(0, 4).equals("Slot")) {
+            this.slots.add(ParseSlot(line));
+        }
+        else{
+            System.out.println("Invalid line was ignored");
+        }
+
+    }
+
+    private String ParseQuestion(String line){
+        String question = line.substring(9);
+        return question;
+    }
+
+    private Tuple<String,String> ParseSlot(String line){
+
+        line = line.substring(5);
+        String[] lineElements = line.split(">");
+        String firstString = lineElements[0].substring(1).trim();
+        String secondString = lineElements[1].trim();
+        Tuple<String,String> result = new Tuple<String,String>(firstString,secondString);
+        
+        return result;
+    }
+
+    private Tuple<String,List<Tuple<String,String>>> ParseAnswer(String line){
+
+        line = line.substring(7);
+
+        String[] lineWords = line.split(" ");
+        String resultString = "";
+        List<Tuple<String,String>> parametersList = new  ArrayList<>();
+        
+        for(int i = 0; i < lineWords.length; i++){
+            if(lineWords[i].startsWith("<")){
+                Tuple<String,String> tuple = new Tuple<String,String>(lineWords[i].substring(1, lineWords[i].length() - 1), lineWords[i+1]);
+                parametersList.add(tuple);
+                i++;
+            }
+            else{
+                resultString += (lineWords[i] + " ");
             }
         }
-        return result;
+        return new Tuple<String,List<Tuple<String,String>>>(resultString,parametersList);
+    }
+
+// ---------- Don't know what to do with this method ----------
+    public String AskQuestion(Tuple<String,List<String>> question){
+        for (Tuple<String,List<String>> answer: answers) {
+            if(answer.y().equals(question.y())){
+                return answer.x();
+            }
+        }
+        return "no answer found";
     }
 }
