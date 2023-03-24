365530045c51ee67deaf83e87b6d206026548de7
lalekosucu <lalekosucu@gmail.com>
2023-03-24 10:29:47 +0100

    Face Detection was updated.

--- src/main/java/org/Project22/FaceDetection/WebCamRec.py
+++ src/main/java/org/Project22/FaceDetection/WebCamRec.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import face_recognition
 
 start_time = time.time()
 
@@ -9,34 +10,45 @@ capture = cv2.VideoCapture(0)
 face_detection = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_detection = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-
-def recognizeFaceAndEyes():
+def detectFaceAndEyes():
     detected = False
 
-    while time.time() - start_time <= 10:
-        # image is the numpy array that represents the image.
+    while time.time() - start_time <= 100:
+
         # ret indicates whether the capture is done successfully.
+        # frame is the numpy array that represents the image.
         ret, frame = capture.read()
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # the algo requires gray scale image to perform classification.
+        # the algorithm requires gray scale image to perform classification.
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+        # detects all the faces in the frame.
         # the second input (1.3) is the scale factor for the case of the pixels of the image are high then the size of
         #   the pixels are reduced by the scale factor -30% in this case-. Using small reduce size decreases the risk of
-        #   missing the detection of faces however it makes algo slower. Using bigger scale factor has the reverse effect.
+        #   missing the detection of faces however it makes the algorithm slower.
+        #   Using bigger scale factor has the reverse effect.
         # the third input - minNeighbors - (5) is the parameter specifying how many neighbors each candidate rectangle
         #   should have to retain it. Higher value = fewer detections but higher quality. Value between 3-6 is good.
         faces = face_detection.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
         for (x, y, w, h) in faces:
             # the rectangle draws rectangles which covers the faces in the image.
-            # image, top left corner, the bottom right, color, thickness
+            # the parameters are: frame which has the image, top left corner, the bottom right, color, thickness.
             cv2.rectangle(frame, (x, y), (x + w, y + h), (50, 200, 0), 4)
 
-            roi_gray = gray[y:y + w, x:x + w]
-            roi_color = frame[y:y + h, x:x + w]
-            eyes = eye_detection.detectMultiScale(roi_gray, 1.3, 5)
+            # rectangleOfImage will be used to focus on specifically inside this rectangle to detect the eyes.
+            rectangleOfImage = gray[y:y + w, x:x + w]
+
+            # the copy of the frame to draw rectangles to cover the eyes however there is a difference between frame and
+            # rectangleOfImage_in_frame: rectangleOfImage_in_frame is the little part of the frame that has the sizes of
+            # rectangles that covers the faces.
+            rectangleOfImage_in_frame = frame[y:y + h, x:x + w]
+
+            # the eyes in the rectangle which covers the detected face, will be detected and the rectangles will be
+            # drawn aon the rectangleOfImage_in_frame to specify eyes.
+            eyes = eye_detection.detectMultiScale(rectangleOfImage, scaleFactor=1.3, minNeighbors=5)
             for (ex, ey, ew, eh) in eyes:
-                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (100, 0, 0), 4)
+                cv2.rectangle(rectangleOfImage_in_frame, (ex, ey), (ex + ew, ey + eh), (100, 0, 0), 4)
 
         cv2.imshow('frame', frame)
 
@@ -53,7 +65,7 @@ def recognizeFaceAndEyes():
 
 
 def isDetected():
-    if recognizeFaceAndEyes():
+    if detectFaceAndEyes():
         print("A human was detected")
     else:
         print("No human was detected around.")
---
+++ src/main/java/org/Project22/Getting/GettingInterface.java
@@ -0,0 +1,4 @@
+package org.Project22.Getting;
+
+public interface GettingInterface {
+}
