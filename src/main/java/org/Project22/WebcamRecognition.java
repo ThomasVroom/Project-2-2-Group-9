c07a5c8067290d8317e1703976fecc1e5caaa039
lalekosucu <lalekosucu@gmail.com>
2023-04-25 23:33:36 +0200

    userName input is passed to the python file.

--- src/main/java/org/Project22/WebcamRecognition.java
+++ src/main/java/org/Project22/WebcamRecognition.java
@@ -1,13 +1,22 @@
 package org.Project22;
 
+import org.Project22.GUI.MenuWindow;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class WebcamRecognition {
 
+    static MenuWindow menuWindow = new MenuWindow();
+    static ArrayList<String> nameList = menuWindow.getName_lists();
+
     public static void main(String[] args) throws IOException {
-        String[] source = {"python3", "src/main/java/org/Project22/FaceDetection/WebCamRec.py"};
+
+        String userName = nameList.get(nameList.size()-1);
+
+        String[] source = {"python3", "src/main/java/org/Project22/FaceDetection/WebCamRec.py", userName};
         Process process = Runtime.getRuntime().exec(source);
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -17,3 +26,4 @@ public class WebcamRecognition {
         }
     }
 }
+
