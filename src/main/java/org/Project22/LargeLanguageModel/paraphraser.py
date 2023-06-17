570948f5f971f3916bcc7ad63e0c843afded2553
Aurelien Giuglaris-Michael <aurelien.giuglarism@gmail.com>
2023-06-17 12:03:07 +0200

    first update paraphraser
    first update not finished yet

--- src/main/java/org/Project22/LargeLanguageModel/paraphraser.py
+++ src/main/java/org/Project22/LargeLanguageModel/paraphraser.py
@@ -2,6 +2,13 @@ import random
 import torch
 import time
 
+# Goal : Fine-tuning the parameters of the hugging-face model and 
+#        try to generate more than 20 paraphrased sentences per input
+
+# Step 1 : Implement a metric score of the paraphrased sentences to try and optimize a number.
+# Step 2 : Implement some sort of grid search that gives the best parameters for the model.
+#          the best parameters are those that maximize the metric score.
+
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -12,14 +19,14 @@ model = AutoModelForSeq2SeqLM.from_pretrained("humarin/chatgpt_paraphraser_on_T5
 
 def paraphrase(
         question,
-        num_beams=50,
-        num_beam_groups=50,
-        num_return_sequences=50,
-        repetition_penalty=10.0,
-        diversity_penalty=3.0,
-        no_repeat_ngram_size=2,
-        temperature=0.7,
-        max_length=128
+        num_beams=20, # num_beams=20
+        num_beam_groups=20, # num_beam_groups=20
+        num_return_sequences=20, # num_return_sequences=20
+        repetition_penalty=13.0, # repetition_penalty=1.5 #10.0
+        diversity_penalty=4.0, # diversity_penalty=0.5 #3.0
+        no_repeat_ngram_size=2, # no_repeat_ngram_size=3 #2
+        temperature=0.7, # temperature=0.7
+        max_length=128 # max_length=128
 ):
     input_ids = tokenizer(
         f'paraphrase: {question}',
@@ -40,11 +47,14 @@ def paraphrase(
     return res
 
 
-
-def paraphrase_20(sentence):
+def paraphrase_time(sentence):
     start_time = time.time()
     result = paraphrase(sentence)
     end_time = time.time()
     execution_time = end_time - start_time
     print("Execution Time:", execution_time, "seconds")
     return result
+
+# Testing 
+if __name__ == '__main__':
+    print(paraphrase_time("I have no idea."))
